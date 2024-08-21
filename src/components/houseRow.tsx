e3e7f83414049c702dee5cd3dbd492d8f6124d83
Vincent Escarcha <131845300+vescarcha@users.noreply.github.com>
2024-08-22 01:16:55 +0800

    Conditional rendering sample

--- src/components/houseRow.tsx
+++ src/components/houseRow.tsx
@@ -2,11 +2,14 @@ import currencyFormatter from "../helpers/currencyFormatter";
 
 export default function HouseRow(props: any) {
   let house = props.house;
+
   return (
     <tr>
       <td>{house.address}</td>
       <td>{house.country}</td>
-      <td>{currencyFormatter.format(house.price)}</td>
+      {house.price && (
+        <td className={`${house.price >= 500000 ? "text-primary" : ""}`}>{currencyFormatter.format(house.price)}</td>
+      )}
     </tr>
   );
 }
